abebdf66af9eea7dd375b9b3dbe2b29d55a6611c
Odelola oluwatobi <129215773+Tflexual@users.noreply.github.com>
2025-11-07 20:19:43 +0100

    Create 0-databaseconnection.py

---
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+class DatabaseConnection:
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn  # gives connection back to use inside 'with'
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()  # always closes connection
