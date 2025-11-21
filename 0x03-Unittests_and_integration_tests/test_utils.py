37d3adaa1e73393fe21e17dd75f37e06876b8398
Odelola oluwatobi <129215773+Tflexual@users.noreply.github.com>
2025-11-21 05:40:08 +0100

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,3 +18,18 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test that access_nested_map returns expected value."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+
+
+
+
+    @parameterized.expand([
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_key):
+        """Test that access_nested_map raises KeyError with correct message."""
+        with self.assertRaises(KeyError) as error:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(error.exception), expected_key)
+
+
