5fbb1fe0418b9e6482d7e9b7e6712e828bd9e0de
Odelola oluwatobi <129215773+Tflexual@users.noreply.github.com>
2025-11-23 22:48:25 +0100

    Create views.py

---
+++ messaging_app/ messaging_app/chats/views.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets
+from .models import Conversation, Message
+from .permissions import IsParticipantOfConversation
+from .serializers import ConversationSerializer, MessageSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsParticipantOfConversation]
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsParticipantOfConversation]
