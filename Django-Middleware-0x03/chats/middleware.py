621bb9a4aaa0e797e1787e820de2b968d2470c36
Odelola oluwatobi <129215773+Tflexual@users.noreply.github.com>
2025-11-23 23:43:57 +0100

    Update middleware.py

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,12 +1,13 @@
-from datetime import datetime
 from django.http import HttpResponseForbidden
 
-class RestrictAccessByTimeMiddleware:
+class RolePermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        current_hour = datetime.now().hour
-        if request.path.startswith('/chats/') and not (18 <= current_hour < 21):  # 6PM-9PM
-            return HttpResponseForbidden("Chat is only available from 6PM to 9PM")
+        restricted_paths = ['/chats/admin/']
+        if any(request.path.startswith(path) for path in restricted_paths):
+            user = request.user
+            if not (user.is_authenticated and (user.is_staff or user.is_superuser)):
+                return HttpResponseForbidden("You do not have permission to access this page")
         return self.get_response(request)
